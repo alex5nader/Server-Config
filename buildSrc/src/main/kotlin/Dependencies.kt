1766ef1de03a58115643835100ff479f9d46744f
Alex Habich <me@alexnader.dev>
2021-01-10 03:21:46 -0600

    Bump version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -36,7 +36,7 @@ object Fabric {
     val api = Dependency("net.fabricmc.fabric-api", "fabric-api", "0.29.3+1.16")
 }
 
-object ServerConfig : Dependency("dev.alexnader", "Server-Config", "0") {
+object ServerConfig : Dependency("dev.alexnader", "Server-Config", "0.1") {
     const val modId = "server_config"
     const val prettyName = "Server Config"
 
